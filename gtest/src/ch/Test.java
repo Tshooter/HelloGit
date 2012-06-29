a11cd0e87f002ecc97f4dda7a411aff8e6958f73
rlo <renato.loeffel@zuehlke.com>
2012-06-29 15:57:18 +0200

    second again

--- gtest/src/ch/Test.java
+++ gtest/src/ch/Test.java
@@ -1,14 +1,9 @@
 package ch;
 
 public class Test {
-	
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		System.out.println("second");
-=======
-		System.out.println("first");
->>>>>>> branch 'master' of https://github.com/Tshooter/HelloGit.git
 	}
-	
+
 }
