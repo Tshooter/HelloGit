5c1d99f72cdb839a95ad0de27add659425ca9a42
rlo <renato.loeffel@zuehlke.com>
2012-06-29 15:56:54 +0200

    first again

--- gtest/src/ch/Test.java
+++ gtest/src/ch/Test.java
@@ -4,11 +4,7 @@ public class Test {
 	
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		System.out.println("second");
-=======
 		System.out.println("first");
->>>>>>> branch 'master' of https://github.com/Tshooter/HelloGit.git
 	}
 	
 }
